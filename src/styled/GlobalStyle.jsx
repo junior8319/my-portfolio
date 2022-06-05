2c569be92893a9b23b2469d80908ba78b4905192
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-06-05 13:38:49 -0300

    styled-components - setup: GlobalStyle - set example styles for the entire  apllication.

---
+++ src/styled/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    background-color: blue;
+    margin: 0;
+    font-family: 'Verdana', sans-serif;
+    height: 100%;
+  };
+`;
+
+export default GlobalStyle;

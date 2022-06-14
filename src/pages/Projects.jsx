c5231a9d7d03d5ba6cb463474692fd13991b067b
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-06-14 19:15:33 -0300

    pages-setup -feat: Projects.jsx set the projects page/component.

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projetos</h1>
+    </div>
+  );
+};
+
+export default Projects;

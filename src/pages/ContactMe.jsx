b021616ceffda2d15e4d648d1bd064d0a06161e6
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-06-14 19:15:15 -0300

    pages-setup -feat: ContactMe.jsx set the contact page/component.

---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const ContactMe = () => {
+  return (
+    <div>
+      <h1>Contato</h1>
+    </div>
+  );
+};
+
+export default ContactMe;

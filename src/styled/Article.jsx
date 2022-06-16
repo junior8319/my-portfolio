25ad4074bf69ee89f8311a9857a3a7563b62f546
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-06-16 20:16:19 -0300

    styles - feat: Article.jsx initial set for the <article> tags.

---
+++ src/styled/Article.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Article = styled.article`
+  width: 70%;
+  padding: 20px 10px;
+  background-color: #ffffff15;
+  border-radius: 10px;
+`;
+
+export default Article;

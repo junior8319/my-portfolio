d0630338c896f2756e80a1ec48a1de8e09808a0e
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-06-17 13:24:21 -0300

    styles - responsiveness - Paragraphs.jsx: decreases font-size on max-width of 400px.

--- src/styled/Paragraphs.jsx
+++ src/styled/Paragraphs.jsx
@@ -5,4 +5,10 @@ export const SimpleP = styled.p`
   background: none;
   width: 65%;
   padding: 10px 0;
+
+  @media (max-width: 400px) {
+    font-size: 0.8rem;
+    margin: auto;
+    width: 100%;    
+  }
 `;

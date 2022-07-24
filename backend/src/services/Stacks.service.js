b5b54fbde8746d5fc5c8b71d2527ec0bf6ded479
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-07-24 08:40:11 -0300

    feature Stacks: createStack - sets the function to register a new stack.

--- backend/src/services/Stacks.service.js
+++ backend/src/services/Stacks.service.js
@@ -10,6 +10,33 @@ const getAllStacks = async () => {
   return stacks.map(stack => stack);
 };
 
+const createStack = async ({
+  title,
+  description,
+  imageUrl,
+  stackDocsUrl,
+  stackUrl,
+}) => {
+  const newStack = await Stack.create({
+    title,
+    description,
+    imageUrl,
+    stackDocsUrl,
+    stackUrl,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  })
+
+  if (!newStack) {
+    return null;
+  }
+
+  console.log(newStack);
+
+  return newStack;
+};
+
 module.exports = {
   getAllStacks,
+  createStack,
 };

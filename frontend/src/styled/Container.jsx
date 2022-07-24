323e3a1be2a520941956bfccd7d531b2427c205a
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-07-24 07:07:52 -0300

    styles update: Container - add hover styles to Articles in Containers.

--- frontend/src/styled/Container.jsx
+++ frontend/src/styled/Container.jsx
@@ -7,6 +7,10 @@ const Container = styled.section`
   border-radius: 10px;
   display: flex;
   margin: 0 auto;
+  article: hover {
+    background-color: #0000ff20;
+    transition: 1s;
+  }
 
   @media (max-width: 700px) {
     flex-wrap: wrap;

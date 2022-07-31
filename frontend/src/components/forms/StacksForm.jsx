8f2931e38356debc83d4e57cafa83839a3fdffad
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-07-31 15:30:18 -0300

    frontend: StacksForm - implementation of stacks register form.

--- frontend/src/components/forms/StacksForm.jsx
+++ frontend/src/components/forms/StacksForm.jsx
@@ -1,29 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { requestStackRegister } from '../../helpers/stacksApi';
 
 const StacksForm = () => {
+  const [stack, setStack] = useState({
+    title: '',
+    description: '',
+    stackDocsUrl: '',
+    imageUrl: '',
+    stackUrl: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setStack({ ...stack, [name]: value });
+  };
+
   return (
     <form action="POST">
+      <label htmlFor="input-title">
+        Título:
+      </label>
+      <input
+        id="input-title"
+        name="title"
+        type="text"
+        value={stack.title}
+        onChange={handleChange}
+      />
+
       <label htmlFor="description-input">
         Descrição:
       </label>
-      <input type="textarea" id="description-input" />
+      <input
+        type="textarea" id="description-input"
+        value={ stack.description }
+        name="description"
+        onChange={handleChange}
+      />
       
       <label htmlFor="docs-url">
         Página da documentação:
       </label>
-      <input type="text" id="docs-url" />
+      <input
+        type="text"
+        id="docs-url"
+        value={stack.stackDocsUrl}
+        name="stackDocsUrl"
+        onChange={handleChange}
+      />
 
       <label htmlFor="image-url">
         URL da imagem:
       </label>
-      <input type="text" id="image-url" />
+      <input
+        type="text"
+        id="image-url"
+        value={stack.imageUrl}
+        name="imageUrl"
+        onChange={handleChange}
+      />
 
       <label htmlFor="stack-url">
         URL da habilidade:
       </label>
-      <input type="text" id="stack-url" />
+      <input
+        type="text"
+        id="stack-url"
+        value={stack.stackUrl}
+        name="stackUrl"
+        onChange={handleChange}
+      />
 
-      <input type="button" value="Salvar" />
+      <input
+        type="button"
+        value="Salvar"
+        onClick={ () => requestStackRegister({
+          body: stack,
+          method: 'POST',
+          url: '/stacks',
+          origin: 'http://localhost:3000',
+        }) }
+      />
     </form>
   );
 }

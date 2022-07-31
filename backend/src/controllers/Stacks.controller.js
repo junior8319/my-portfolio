07dac76d320c7ff6c7d9d6b9e8bef67bd874fa12
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-07-31 15:37:35 -0300

    backend - controller: Stacks - implements the function createStack.

--- backend/src/controllers/Stacks.controller.js
+++ backend/src/controllers/Stacks.controller.js
@@ -17,6 +17,23 @@ const getAllStacks = async (_req, res) => {
   }
 };
 
+const createStack = async (req, res) => {
+  try {
+    const newStack = await stacksService.createStack(req.body);
+    if (!newStack) {
+      return res.status(400).json({
+        message: 'Stack not created',
+      });
+    }
+
+    return res.status(201).json({ message: 'Salvo com sucesso.', stack: newStack });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = {
   getAllStacks,
+  createStack,
 };

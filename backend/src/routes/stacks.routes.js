3451cee7bb4f22137bc72323e5ce0e797b5df1e9
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-08-01 11:38:40 -0300

    backend routes: CRUD functions - implementation of routes: 'PUT', to updateStack; 'DELETE', to deleteStack.

--- backend/src/routes/stacks.routes.js
+++ backend/src/routes/stacks.routes.js
@@ -5,6 +5,8 @@ const stacksRouter = express.Router();
 
 stacksRouter.get('/', stacksController.getAllStacks);
 stacksRouter.post('/', stacksController.createStack);
+stacksRouter.put('/:id', stacksController.updateStack);
+stacksRouter.delete('/:id', stacksController.deleteStack);
 
 module.exports = {
   stacksRouter,

daa1ac84590e055ca8c227c8477db8b687dc1bb1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-08-01 11:37:07 -0300

    backend services: CRUD functions - implementation of: findStackById; updateStack; deleteStack.

--- backend/src/services/Stacks.service.js
+++ backend/src/services/Stacks.service.js
@@ -19,7 +19,44 @@ const createStack = async (stack) => {
   return newStack;
 };
 
+const findStackById = async (id) => {
+  const stack = await Stack.findByPk(id);
+
+  if (!stack) {
+    return null;
+  }
+
+  return stack;
+};
+
+const updateStack = async (id, stack) => {
+  const stackToUpdate = await findStackById(id);
+
+  const updatedStack = await stackToUpdate.update(stack);
+
+  if (!updatedStack) {
+    return null;
+  }
+
+  return updatedStack;
+};
+
+const deleteStack = async (id) => {
+  const stackToDelete = await findStackById(id);
+
+  const deletedStack = await stackToDelete.destroy();
+
+  if (!deletedStack) {
+    return null;
+  }
+
+  return deletedStack;
+};
+
 module.exports = {
   getAllStacks,
   createStack,
+  findStackById,
+  updateStack,
+  deleteStack,
 };

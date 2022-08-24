65205a78d92bc464f56e8028d97bfe59bdc3ccd0
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-08-24 05:40:57 -0300

    frontend - requests - register: Changes the http request for register a Stack to use fetch API.

--- frontend/src/helpers/stacksApi.js
+++ frontend/src/helpers/stacksApi.js
@@ -1,4 +1,5 @@
 const API_URL = process.env.REACT_APP_BASE_URL;
+const API_ORIGIN = process.env.REACT_APP_BASE_URL_ORIGIN;
 
 const getStacks = async () => {
   try {
@@ -11,18 +12,24 @@ const getStacks = async () => {
   }
 };
 
-const requestStackRegister = async (receivedData) => {
+const requestStackRegister = (receivedData) => {
   try {
-    const httpRequest = new XMLHttpRequest();
-    if (httpRequest) {
-      httpRequest.open(receivedData.method, API_URL + receivedData.url);
-      httpRequest.setRequestHeader('Content-Type', 'application/json');
-      httpRequest.setRequestHeader('Accept', 'application/json');
-      httpRequest.setRequestHeader('Access-Control-Allow-Origin', receivedData.origin);
-      httpRequest.onreadystatechange = () => console.log(httpRequest);
-      httpRequest.send(JSON.stringify(receivedData.body));
-    }
-    
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(receivedData),
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': API_ORIGIN,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    };
+    const response = fetch(`${API_URL}/stacks`, options)
+      .then(response => response.json())
+      .then(data => {
+        return data.stack;
+      });
+    return response;
   } catch (error) {
     console.log(error);
     return new Error(`Something went wrong. Error: ${error}`);
@@ -37,7 +44,6 @@ const updateStackRequest = async (receivedData) => {
       httpRequest.setRequestHeader('Content-Type', 'application/json');
       httpRequest.setRequestHeader('Accept', 'application/json');
       httpRequest.setRequestHeader('Access-Control-Allow-Origin', receivedData.origin);
-      httpRequest.onreadystatechange = () => console.log(httpRequest);
       httpRequest.send(JSON.stringify(receivedData.body));
     }
     

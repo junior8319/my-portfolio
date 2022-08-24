c307c4fbdd74bb05278d6d5cc43244fbeaa82b46
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-08-24 05:44:01 -0300

    stacks feature - context API: Sets the provider of StacksContext.

--- frontend/src/context/StacksProvider.jsx
+++ frontend/src/context/StacksProvider.jsx
@@ -4,19 +4,29 @@ import StacksContext from './StacksContext';
 
 const StacksProvider = ({ children }) => {
   const [stacks, setStacks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getStacksFromApi = async () => {
+    const data = await getStacks();
+    setStacks(data);
+    return data;
+  };
 
   useEffect(() => {
-    getStacks()
-      .then(data => setStacks(data));
+    getStacksFromApi();
   }, []);
 
-  useEffect(() => {}, [stacks]);
-
-  console.log(stacks);
+  useEffect(() => {
+    setLoading(isLoading => !isLoading);
+  }, [stacks]);
+  let mappedStacks = stacks.map((stack) => stack);
 
   const contextValue = {
     stacks,
-    setStacks
+    mappedStacks,
+    loading,
+    setStacks,
+    getStacksFromApi,
   }
 
   return (

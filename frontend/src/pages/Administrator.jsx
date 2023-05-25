422340211b84b28677cac383e9481d89fc170a22
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-25 11:22:55 -0300

    frontend - pages - Administrator - change: Conditional rendering of StacksForm.

--- frontend/src/pages/Administrator.jsx
+++ frontend/src/pages/Administrator.jsx
@@ -2,23 +2,38 @@ import React from 'react';
 import StacksForm from '../components/forms/StacksForm';
 import NavBar from '../components/NavBar';
 import StacksTable from '../components/tables/StacksTable';
-import StacksProvider from '../context/StacksProvider';
+// import StacksProvider from '../context/StacksProvider';
 import Article from '../styled/Article';
 import Container from '../styled/Container';
-import { Title1 } from '../styled/Titles';
+import { Title1, Title2 } from '../styled/Titles';
+import { useContext } from 'react';
+import StacksContext from '../context/StacksContext';
 
 const Administrator = () => {
+  const { isUpdating } = useContext(StacksContext);
   return (
-    <StacksProvider>
+    // <StacksProvider>
+    <>
       <NavBar />
       <Container>
         <Article>
           <Title1>Administrador</Title1>
-          <StacksForm />
+          {
+            !isUpdating
+            ?
+            (
+              <StacksForm />
+            )
+            :
+            (
+              <Title2>Atualizando uma habilidade.</Title2>
+            )
+          }
           <StacksTable />
         </Article>
       </Container>
-    </StacksProvider>
+    </>
+    // </StacksProvider>
 
   );
 }

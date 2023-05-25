015ce63f770342b45da2a3f547e383680d1bbeb1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-25 11:44:18 -0300

    backend - routes - Stacks - feature: Adds a route to get a stack by its id.

--- backend/src/routes/stacks.routes.js
+++ backend/src/routes/stacks.routes.js
@@ -4,6 +4,7 @@ const stacksController = require('../controllers/Stacks.controller');
 const stacksRouter = express.Router();
 
 stacksRouter.get('/', stacksController.getAllStacks);
+stacksRouter.get('/:id', stacksController.getStackById);
 stacksRouter.post('/', stacksController.createStack);
 stacksRouter.put('/:id', stacksController.updateStack);
 stacksRouter.delete('/:id', stacksController.deleteStack);

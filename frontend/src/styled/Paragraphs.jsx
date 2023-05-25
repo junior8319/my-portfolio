94ee54bbb0b405fb3b188f49cd0cfa3190d0c023
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-25 11:57:35 -0300

    styles - Paragraphs component - change: Reduces a little of the text size of paragraphs.

--- frontend/src/styled/Paragraphs.jsx
+++ frontend/src/styled/Paragraphs.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const SimpleP = styled.p`
-  font-size: 1rem;
+  font-size: 0.8rem;
   background: none;
   width: 65%;
   padding: 10px 0;
 
   @media (max-width: 400px) {
-    font-size: 0.8rem;
+    font-size: 0.6rem;
     margin: auto;
     width: 100%;    
   }

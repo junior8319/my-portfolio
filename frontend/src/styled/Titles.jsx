12a8c122fc1d8c60a1dfb206cedf883f282bddc3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-25 11:59:13 -0300

    styles - Titles component - change: Reduces a little of the text size of tittle 1. feature: Adds Title2 (a little smaller title).

--- frontend/src/styled/Titles.jsx
+++ frontend/src/styled/Titles.jsx
@@ -1,12 +1,23 @@
 import styled from 'styled-components';
 
 export const Title1 = styled.h1`
-  font-size: 2.0rem;
+  font-size: 1.6rem;
   background: none;
   padding: 10px 0;
 
   @media (max-width: 400px) {
-    font-size: 1.3rem;
+    font-size: 1.1rem;
+    width: 100%;
+  }
+`;
+
+export const Title2 = styled.h2`
+  font-size: 1.2rem;
+  background: none;
+  padding: 10px 0;
+
+  @media (max-width: 400px) {
+    font-size: 0.9rem;
     width: 100%;
   }
 `;

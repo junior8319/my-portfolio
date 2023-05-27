b1af87721f60c7f6c8c895a3c9847c9477126555
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-27 19:46:04 -0300

    styles - stacks context - CardPicture Adds CardPicture to ilustrate the exhibition of skills (stacks).

--- frontend/src/styled/Pictures.jsx
+++ frontend/src/styled/Pictures.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const NormalPicture = styled.img`
+export const NormalPicture = styled.img`
   max-width: 175px;
   max-height: 250px;
   background: none;
@@ -20,4 +20,18 @@ const NormalPicture = styled.img`
   }
 `;
 
-export default NormalPicture;
+export const CardPicture = styled.img`
+  width: 100px;
+  background: none;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 100, 0.5);
+
+  @media (max-width: 700px) {
+    justify-self: center;
+    text-align: center;
+    align-self: center;
+    margin: auto;
+    width: 100%;
+    height: auto;
+  }
+`;

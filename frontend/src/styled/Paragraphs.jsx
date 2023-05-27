d1ad0a15bcac0d63f4ad6b319f44fd01154181d3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-27 19:48:52 -0300

    styles - stacks context - SimpleP Reduces a paragraphs size a litle bit.

--- frontend/src/styled/Paragraphs.jsx
+++ frontend/src/styled/Paragraphs.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const SimpleP = styled.p`
-  font-size: 0.8rem;
+  font-size: 0.9rem;
   background: none;
   width: 65%;
   padding: 10px 0;
 
   @media (max-width: 400px) {
-    font-size: 0.6rem;
+    font-size: 0.7rem;
     margin: auto;
     width: 100%;    
   }

435838ee755a8556677d4f502393dd995a15d629
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-30 16:36:46 -0300

    frontend - style features  - scroll bars: Sets global styles for scroll bars.

--- frontend/src/styled/GlobalStyle.jsx
+++ frontend/src/styled/GlobalStyle.jsx
@@ -12,6 +12,21 @@ const GlobalStyle = createGlobalStyle`
     color: #488AFA;
     transition: 1s;
   }
+
+  *::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+  *::-webkit-scrollbar-track {
+    background-color: #ffffff15;
+    border: 0.5px solid #b9d6f420;
+    border-radius: 15px;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #0000ff20;
+    border-radius: 15px;
+  }
 `;
 
 export default GlobalStyle;

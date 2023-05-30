73731889833d2f08dc6153456acc655b66ce36a2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-05-30 16:17:47 -0300

    frontend - changes - styles - Container: Sets container height to 90% viewport hieght;

--- frontend/src/styled/Container.jsx
+++ frontend/src/styled/Container.jsx
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 const Container = styled.section`
   width: 98%;
+  height: 90vh;
   position: relative;
   top: 55px;
   border-radius: 10px;
   display: flex;
   margin: 0 auto;
-  article: hover {
+
+  article:hover {
     background-color: #0000ff20;
     transition: 1s;
   }

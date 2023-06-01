c3314b61a7e20ff2d3d090ac0eda06b88c97d414
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-01 16:25:57 -0300

    frontend - features - styles - Buttons: Set styles for button elements.

---
+++ frontend/src/styled/Buttons.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const ControlButton = styled.button`
+  border: none;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 10px;
+  width: 100px;
+  background-color: #0000ff20;
+  color: #b9d6f4;
+
+  &:hover {
+    background-color: #b9d6f4;
+    color: #47425f;
+  }
+`;
+
+export const SaveButton = styled.input`
+  border: none;
+  font-weight: bold;
+  font-size: 0.8rem;
+  padding: 10px;
+  border-radius: 10px;
+  width: 100px;
+  background-color: #13890f60;
+  color: #e1dbdb90;
+  
+  &:hover {
+    background-color: #13890f;
+    color: #e1dbdb;
+  }
+`;
+
+export const CancelButton = styled.input`
+  border: none;
+  font-weight: bold;
+  font-size: 0.8rem;
+  padding: 10px;
+  border-radius: 10px;
+  width: 100px;
+  background-color: #89250f60;
+  color: #e1dbdb90;
+  
+  &:hover {
+    background-color: #89250f;
+    color: #e1dbdb;
+  }
+`;

562f41c00f2ddf41ed1459f9edf3bc047cf1ac37
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-01 16:23:40 -0300

    frontend - changes- styles - ControlBar: Changes on colors and width.

--- frontend/src/styled/ControlBar.jsx
+++ frontend/src/styled/ControlBar.jsx
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
 const ControlBar = styled.nav`
-  width: 98%;
+  width: 100%;
   height: 10%;
   padding: 5px;
-  background-color: #ffffff00;
+  background-color: #ffffff15;
   border-radius: 10px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-around;
   margin: 0;
+
+  &:hover {
+    background-color: #0000ff20;
+  }
 
   @media (max-width: 400px) {
     align-self: center;

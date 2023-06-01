c29c35467c18ba9813601b2ea5f4068cea44f4d9
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-01 16:03:55 -0300

    frontend - changes - styles - Link: Change the color of a Link's text; Adds a hover behavior to Link styled component.

--- frontend/src/styled/Link.jsx
+++ frontend/src/styled/Link.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 const Link = styled.a`
-  color: #853935;
+  color: ${props => props.color || "#853935"};
   text-decoration: none;
   background: none;
+
+  &:hover {
+    color: #b9d6f4;
+  }
 `;
 
 export default Link;

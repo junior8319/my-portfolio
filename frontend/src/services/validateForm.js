b8452f0c0a9bf6c8888573ec210524b52a3d30bd
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-07 13:55:54 -0300

    frontend - features - contact page - styles - validations: Sets helpers validation functions to handle contact form filling.

---
+++ frontend/src/services/validateForm.js
@@ -0,0 +1,48 @@
+export const validateName = (receivedNameString) => {
+  if (typeof receivedNameString !== 'string') {
+    return 'Este campo deve receber uma string';
+  }
+
+  if (receivedNameString.length <= 3) {
+    return 'O nome deve ter mais de três caracteres.';
+  }
+
+  return '';
+};
+
+export const validatePhone = (receivedPhone) => {
+  const VALID_NUMBER = /[0-9]{9,14}/gm;
+  const IS_VALID = VALID_NUMBER.test(receivedPhone);
+
+  return (IS_VALID)
+  ?
+    ''
+  :
+    'Favor informar um número de telefone válido (digite somente números).';
+};
+
+export const validateEmail = (receivedEmailString) => {
+  const VALID_EMAIL = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm;
+  const IS_VALID = VALID_EMAIL.test(receivedEmailString);
+
+  return (IS_VALID)
+  ?
+    ''
+  :
+    (
+      'Favor informar um endereço de e-mail'
+    +
+      ' válido (ex. "seuIdentificador@dominio.com.br").'
+    );
+};
+
+export const validateMessage = (receivedMsgString) => {
+  const VALID_MESSAGE = /\p{L}+/gu;
+  const IS_VALID = receivedMsgString.match(VALID_MESSAGE);
+
+  return (IS_VALID !== null && IS_VALID.length > 0)
+  ?
+    ''
+  :
+    'Esta não é uma mensagem válida, favor redigir com palavras legíveis.';
+};

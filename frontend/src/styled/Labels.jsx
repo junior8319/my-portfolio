75c015219df778b5c8601236d36bd4fe16e2c084
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-12 06:47:45 -0300

    frontend - fixes - responsivity - Labels.jsx: Fix layout and fonts sizes on max width of 1100px displays.

--- frontend/src/styled/Labels.jsx
+++ frontend/src/styled/Labels.jsx
@@ -4,10 +4,18 @@ export const Label = styled.label`
   font-size: 1rem;
   width: 95%;
   background-color: transparent;
+
+  @media (max-width: 1100px) {
+    font-size: 0.85rem;
+  }
 `;
 
 export const Span = styled.span`
   font-size: 1.3rem;
   color: #f59a9a;
   background-color: transparent;
+
+  @media (max-width: 1100px) {
+    font-size: 1.1rem;
+  }
 `;

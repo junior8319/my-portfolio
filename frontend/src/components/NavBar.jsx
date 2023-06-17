f2080ad3bf6d15dfb545304fc466a6f8b8489f58
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-17 19:50:38 -0300

    frontend - fixes - responsive navbar: Navbar.jsx Removal of 'NavBar' word;

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,6 @@ import StyledNavBar from '../styled/NavBar';
 const NavBar = () => {
   return (
     <StyledNavBar>
-        <h1>NavBar</h1>
         <Link to="/">Home</Link>
         <Link to="/contact-me">Contato</Link>
         <Link to="/articles">Artigos</Link>

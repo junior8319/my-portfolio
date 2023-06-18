cc33bf1af8bbe28fb894b50252792d845ddd709f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-18 12:47:51 -0300

    frontend - fixes - responsive nav bar: component Navbar.jsx: Adds nav bar menu button to show/hide the nav bar at small screens.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import StyledNavBar from '../styled/NavBar';
+import { NavMenuButton } from '../styled/Buttons';
 
 const NavBar = () => {
   return (
-    <StyledNavBar>
-        <Link to="/">Home</Link>
-        <Link to="/contact-me">Contato</Link>
-        <Link to="/articles">Artigos</Link>
-        <Link to="/projects">Projetos</Link>
-        <Link to="/administrator">Administrador</Link>
-    </StyledNavBar>
+    <div>
+      <NavMenuButton>Menu</NavMenuButton>
+      <StyledNavBar>
+          <Link to="/">Home</Link>
+          <Link to="/contact-me">Contato</Link>
+          <Link to="/articles">Artigos</Link>
+          <Link to="/projects">Projetos</Link>
+          <Link to="/administrator">Administrador</Link>
+      </StyledNavBar>
+    </div>
   );
 }
 

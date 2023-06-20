5a7a4e91e24a8d763b6bbe526d2f0ecb9c26bf6a
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-20 06:21:56 -0300

    frontend - features - responsive nav bar: NavBar.jsx: Sets conditional rendering between NavMenu and StyledNavBar.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import StyledNavBar from '../styled/NavBar';
 import { NavMenuButton } from '../styled/Buttons';
+import { useState } from 'react';
+import NavMenu from './NavMenu';
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuBtnClick = () => {
+    console.log('Click');
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div>
-      <NavMenuButton>Menu</NavMenuButton>
-      <StyledNavBar>
-          <Link to="/">Home</Link>
-          <Link to="/contact-me">Contato</Link>
-          <Link to="/articles">Artigos</Link>
-          <Link to="/projects">Projetos</Link>
-          <Link to="/administrator">Administrador</Link>
-      </StyledNavBar>
+      {
+        showMenu
+        ?
+        <>
+          <NavMenuButton
+            onClick={ handleMenuBtnClick }
+          >
+            Fechar
+          </NavMenuButton>
+          <NavMenu />
+        </>
+        :
+        <>
+          <NavMenuButton
+            onClick={handleMenuBtnClick}
+          >
+            Menu
+          </NavMenuButton>
+          <StyledNavBar>
+            <Link to="/">Home</Link>
+            <Link to="/contact-me">Contato</Link>
+            <Link to="/articles">Artigos</Link>
+            <Link to="/projects">Projetos</Link>
+            <Link to="/administrator">Administrador</Link>
+          </StyledNavBar>
+        </>
+      }
     </div>
   );
 }

1cb93039766661a0399a4ccd55e742f969bf446f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-21 07:11:14 -0300

    backend - features - models - Project: Sets project model. basic CLI: 'npx sequelize-cli model:create --name Project  --attributes title:string' (also create the migration).

---
+++ backend/src/database/models/Project.js
@@ -0,0 +1,65 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+const { Stack } = require('./Stack.js');
+module.exports = (sequelize, DataTypes) => {
+  class Project extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  Project.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    snapshot: {
+      type: DataTypes.TEXT,
+      defaultValue: '',
+    },
+    description: {
+      type: DataTypes.TEXT,
+      defaultValue: '',
+      allowNull: false,
+    },
+    projectUrl: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+    },
+    startDate: {
+      type: DataTypes.DATE,
+    },
+    finishDate: {
+      type: DataTypes.DATE,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updateAt: {
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'Project',
+    tableName: 'projects',
+    underscored: true,
+  });
+
+  Project.Stack = Project.hasMany(Stack);
+
+  return Project;
+};

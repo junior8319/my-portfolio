b2a022ad18b35b22e12e800f48d85495484f2da1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-23 19:03:05 -0300

    backend - feat - services - Projects: Feature - getProjectById service function.

--- backend/src/services/Projects.service.js
+++ backend/src/services/Projects.service.js
@@ -10,6 +10,15 @@ const getAllProjects = async () => {
   return projects.map(project => project);
 };
 
+const getProjectById = async (id) => {
+  const project = await Project.findByPk(Number(id));
+
+  if (!project) return null;
+
+  return project.dataValues;
+};
+
 module.exports = {
   getAllProjects,
+  getProjectById,
 };

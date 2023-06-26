64488c21061394c979fea653b061c22453562e7f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-26 08:17:18 -0300

    backend - feat. -  projects router - server app: feat - add projects routes to server app.

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { stacksRouter } = require('../routes/index.routes');
+const { stacksRouter, projectsRouter } = require('../routes/index.routes');
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 app.use(cors()); // allow all origins
 
 app.use('/stacks', stacksRouter);
+app.use('/projects', projectsRouter);
 
 module.exports = app;

93c81303300f072f327017d5348fa5efea0e22a4
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-06-27 08:47:31 -0300

    backend- fix- models- StackProject: underscored: true.

--- backend/src/database/models/StackProject.js
+++ backend/src/database/models/StackProject.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'StackProject',
     tableName: 'stacksProjects',
+    underscored: true,
   });
   return StackProject;
 };

1ae768f1fb702e270f49a2715a8dc57ac0d58360
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-01 19:00:05 -0300

    backend - fix - models - Stack, Project: Fix - gives associative table name as string.

--- backend/src/database/models/Stack.js
+++ backend/src/database/models/Stack.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) { // 
-      Stack.belongsToMany(models.Project, { through: models.StackProject });
+      Stack.belongsToMany(models.Project, { through: 'stacksProjects' });
     }
   }
   Stack.init({

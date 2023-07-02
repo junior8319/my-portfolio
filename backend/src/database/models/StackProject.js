f81374f236501b99d333ffcc34f8d297caf80213
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-02 19:44:28 -0300

    backend - fix - models - StackProject: Fix - removal of associations wrong placed.

--- backend/src/database/models/StackProject.js
+++ backend/src/database/models/StackProject.js
@@ -9,10 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      StackProject.belongsTo(models.Project);
-      StackProject.belongsTo(models.Stack);
-    }
+    // static associate(models) {
+    
+    // }
   }
   StackProject.init({
     stackId: {

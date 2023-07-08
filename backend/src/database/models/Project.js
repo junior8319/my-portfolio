205275f202cfc4d91b94cf98a9c8cec61e8b3256
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-08 08:44:56 -0300

    back - fix - models - Project - removal of asso- ciation alias.

--- backend/src/database/models/Project.js
+++ backend/src/database/models/Project.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Project.belongsToMany(models.Stack, { through: models.StackProject, as: 'stacks' });
+      Project.belongsToMany(models.Stack, { through: models.StackProject });
     }
   }
   Project.init({

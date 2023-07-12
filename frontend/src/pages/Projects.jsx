515401781f48ce09f6453328a9e2eaae12c5036c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-12 18:50:15 -0300

    frontend - feat. - sets Projects page.

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import Container from '../styled/Container';
+import { Title1 } from '../styled/Titles';
+import ProjectsComponent from '../components/ProjectsComp';
 
 const Projects = () => {
   return (
-    <div>
+    <>
       <NavBar />
-      <h1>Projetos</h1>
-    </div>
+      <Container>
+        <Title1>Meus Projetos</Title1>
+        <ProjectsComponent />
+      </Container>
+    </>
   );
 };
 

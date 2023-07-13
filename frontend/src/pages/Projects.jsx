109263287cb37375ee104a39c475b78123a29c6c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-13 17:34:52 -0300

    frontend - fix - Projects page - fix: page title.

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -3,13 +3,20 @@ import NavBar from '../components/NavBar';
 import Container from '../styled/Container';
 import { Title1 } from '../styled/Titles';
 import ProjectsComponent from '../components/ProjectsComp';
+import Article from '../styled/Article';
 
 const Projects = () => {
   return (
     <>
       <NavBar />
       <Container>
-        <Title1>Meus Projetos</Title1>
+        <Article
+          width={ '90%' }
+          backgroundColor={ 'transparent' }
+        >
+          <Title1>Meus Projetos</Title1>
+        </Article>
+
         <ProjectsComponent />
       </Container>
     </>

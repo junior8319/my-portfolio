1e97a371f6cee93246216be803641fdb837e6430
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-18 07:44:36 -0300

    frontend - helpers - handleCompatibility.js: feat: handleDateCompatibility helper function.

---
+++ frontend/src/helpers/handleCompatibility.js
@@ -0,0 +1,10 @@
+
+export const handleDateCompatibility = (date) => {
+  date = new Date(date);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month}-${day}`;
+};
+

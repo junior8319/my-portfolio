fbf141e971f723933ae49b0250791336a22265c9
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-19 08:24:59 -0300

    frontend - helpers - fix: compatibility of months and days less than 10.

--- frontend/src/helpers/handleCompatibility.js
+++ frontend/src/helpers/handleCompatibility.js
@@ -5,6 +5,12 @@ export const handleDateCompatibility = (date) => {
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
+  if (month < 10 && day < 10) return `${year}-0${month}-0${day}`;
+
+  if (month < 10) return `${year}-0${month}-${day}`;
+
+  if (day < 10) return `${year}-${month}-0${day}`;
+
   return `${year}-${month}-${day}`;
 };
 

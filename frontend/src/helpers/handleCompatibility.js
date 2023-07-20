1a5d76bf9eee42c04cb76fef4057cbe67181951c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-07-20 19:59:09 -0300

    frontend-helpers-handleCompatibility.js - fix & feat: fix: changes to getUTC Date methods to correct timezone discrepancy; feat: adds handleTimezoneCompatibility function to format date in Brazillian way.

--- frontend/src/helpers/handleCompatibility.js
+++ frontend/src/helpers/handleCompatibility.js
@@ -1,8 +1,8 @@
 export const handleDateCompatibility = (date) => {
   date = new Date(date);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
 
   if (month < 10 && day < 10) return `${year}-0${month}-0${day}`;
 
@@ -13,3 +13,17 @@ export const handleDateCompatibility = (date) => {
   return `${year}-${month}-${day}`;
 };
 
+export const handleTimeZoneCompatibility = (date) => {
+  const dateObj = new Date(date);
+  const day = dateObj.getUTCDate();
+  const month = dateObj.getUTCMonth() + 1;
+  const year = dateObj.getUTCFullYear();
+  
+  if (month < 10 && day < 10) return `0${day}/0${month}/${year}`;
+
+  if (month < 10) return `${day}/0${month}/${year}`;
+
+  if (day < 10) return `0${day}/${month}/${year}`;
+
+  return `${day}/${month}/${year}`;
+};
